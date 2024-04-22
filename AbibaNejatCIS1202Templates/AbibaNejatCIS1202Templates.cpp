35d560189bd77a54ae3bcbfa680f7a5f906e03e4
Nejat Abiba <nejatabiba@gmail.com>
2024-04-22 16:44:24 -0500

    Added template half method. Added driver to main to test half method.

--- AbibaNejatCIS1202Templates/AbibaNejatCIS1202Templates.cpp
+++ AbibaNejatCIS1202Templates/AbibaNejatCIS1202Templates.cpp
@@ -5,8 +5,22 @@
 
 #include <iostream>
 
+using namespace std;
+
+template <class T> T half(const T& num);
+
 int main()
 {
-    
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+
 }
 
+template <class T> T half(const T& num) {
+    return num / 2;
+}
